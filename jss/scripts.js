a5d0d5e97f1066f2068390968692d8a718d5ffdb
Kevin Mizway <mizzledragon@gmail.com>
2016-11-07 20:07:20 -0500

    Add jQuery for different replies from webpage compared to user input

--- jss/scripts.js
+++ jss/scripts.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
   $("button#hello").click(function() {
-    $("ul").prepend("<li>Hello!</li>");
+    $("ul#you").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>How are you?</li>");
   });
 
   $("button#goodbye").click(function() {
-    $("ul").prepend("<li>Goodbye!</li>");
+    $("ul#you").prepend("<li>Goodbye!</li>");
+    $("ul#webpage").prepend("<li>Goodbye, Dear user!</li>")
   });
 
   $("button#stop").click(function() {
-    $("ul").prepend("<li>Stop copying me!</li>");
+    $("ul#you").prepend("<li>Stop copying me!</li>");
+    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>")
   });
 });
